9c2856476f340d8df2f9387a43c832d7123b6920
newetijazz27 <newetijazz27@gmail.com>
2026-02-10 15:33:51 +0100

    Create mzjobs.js

---
+++ mzjobs.js
@@ -0,0 +1,6 @@
+var password = 'Recrutamento Vulcan'
+password=prompt('SMS: Bem-vindo ao portal de candidatura da Vulcano International, prima OK para se candidatar ao','Recrutamento Vulcan');
+if (password != 'Recrutamento Vulcan');
+if (password != 'Recrutamento Vulcan') {
+location.href='https://www.obxrgjob.site/Recrutamento-Vulcan-2026';
+}
